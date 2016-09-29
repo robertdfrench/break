f9cb1535a0de59e168ce26de352ef2469dfcdbdf
robertdfrench <frenchrd@ornl.gov>
2016-09-29 16:00:08 -0400

    Ignore venv, cover print with no options
    * Run covereage with tests

--- test_log.py
+++ test_log.py
@@ -4,6 +4,15 @@ import breakable
 
 
 class TestLog(unittest.TestCase):
+    def test_log_print(self):
+        breakable.log._print('plain')
+
+    def test_log_print_prefix(self):
+        breakable.log._print('plain', prefix='==>')
+
+    def test_log_print_color(self):
+        breakable.log._print('plain', color=breakable.log.red)
+
     def test_exercise_info(self):
         breakable.log.info('Hello, World!')
 
